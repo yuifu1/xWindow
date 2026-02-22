0e7651ba85900338c910bd3288a9bd29eca07d5e
yuiful <yui9@msn.com>
2026-02-23 06:43:26 +0900

    test iikanji

--- nishigori.c
+++ nishigori.c
@@ -1,29 +1,31 @@
-#include <X11/Xft/Xft.h>
-#include <X11/Xlib.h>
-#include <X11/Xlocale.h>
-#include <X11/Xutil.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xlocale.h>
+#include <X11/Xft/Xft.h>
 #include <unistd.h>
 
 #define BORDER 2
 #define WIDTH 500
 #define HIGHT 350
 
-#define BALL 50
-#define MAX_Speed 3  // 3程度を推奨
+#define BALL 50            //初期の円の数と
+#define MAX_BALL 1000      //最大個数
+#define MAX_Speed 3        //3程度を推奨
 #define R 8.3
 
 void ball(int, int, int);
 
-int x_down = 100, y_down = 100; // 箱の下限
-const int x_up = 800, y_up = 800; // 箱の上限
-const int large = 20; // 円の大きさ
+int x_down = 100, y_down = 100; //箱の下限
+const int x_up = 800, y_up = 800; //箱の上限
+const int large = 20; //円の大きさ
 int mode = 1;
-int T = 100;
+int T = 273;
+int mol = BALL;
 
-int x_prot[BALL + 1], y_prot[BALL + 1], x_vector[BALL + 1], y_vector[BALL + 1];
+int x_prot[MAX_BALL + 1], y_prot[MAX_BALL + 1], x_vector[MAX_BALL + 1], y_vector[MAX_BALL + 1];
 int command[10];
 
 Display* dpy;
@@ -32,15 +34,12 @@ Window root;
 int screen;
 unsigned long black, white;
 GC gc;
-Colormap cmap; // カラーマップ構造体
-XColor c0, c1; // カラー構造体
-
-char message[] = "DVD";
 
 void main(int argc, char** argv) {
-	char p_value[100], v_value[100], n_value[100], t_value[100];
+	char p_value[100], v_value[100], n_value[100], r_value[100], t_value[100];
 
-	for(int i = 0; i < BALL + 1; i++) {
+
+	for(int i = 0; i < MAX_BALL + 1; i++) {
 		x_prot[i] = rand() % (x_up - x_down - large) + x_down;
 		y_prot[i] = rand() % (y_up - y_down - large) + y_down;
 		if(i % 2 == 1) {
@@ -59,8 +58,8 @@ void main(int argc, char** argv) {
 	white = WhitePixel(dpy, screen);
 	black = BlackPixel(dpy, screen);
 
-	w = XCreateSimpleWindow(dpy, root, 100, 100, WIDTH, HIGHT, BORDER, black,
-	                        white);
+
+	w = XCreateSimpleWindow(dpy, root, 100, 100, WIDTH, HIGHT, BORDER, black, white);
 
 	gc = XCreateGC(dpy, w, 0, NULL);
 
@@ -68,40 +67,60 @@ void main(int argc, char** argv) {
 	XftColor xftcolor;
 	XftFont* xftfont;
 
-	xftdraw = XftDrawCreate(dpy, w, DefaultVisual(dpy, screen),
-	                        DefaultColormap(dpy, screen));
+	xftdraw = XftDrawCreate(dpy, w,DefaultVisual(dpy, screen),DefaultColormap(dpy, screen));
 
 	XRenderColor xrcolor = {0, 0, 0, 0xffff};
-	XftColorAllocValue(dpy, DefaultVisual(dpy, screen),
-	                   DefaultColormap(dpy, screen), &xrcolor, &xftcolor);
+	XftColorAllocValue(dpy, DefaultVisual(dpy, screen),DefaultColormap(dpy, screen), &xrcolor, &xftcolor);
 
 	xftfont = XftFontOpenName(dpy, screen, "Sans:size=32");
 
 	XMapWindow(dpy, w);
 
-	XSelectInput(dpy, w, KeyPressMask | ButtonPressMask);
+	XSelectInput(dpy, w,KeyPressMask | ButtonPressMask);
 	XEvent event;
 
 	while(1) {
-		// イベント処理
+		//イベント処理
 		while(XPending(dpy)) {
 			XNextEvent(dpy, &event);
 
 			switch(event.type) {
 				case KeyPress:
-					if(event.xkey.keycode == 38)
-						if(y_up - y_down <= large + MAX_Speed * 2 ||
-							x_up - x_down <= large + MAX_Speed * 2);
+					if(event.xkey.keycode == 39) //lを押す
+						if(y_up - y_down <= large + (MAX_Speed * T / 100) * 2 || x_up - x_down <= large + (MAX_Speed * T
+							/ 100) * 2);
 						else {
 							y_down++;
 							x_down++;
 						}
-					else if(event.xkey.keycode == 56)
+					else if(event.xkey.keycode == 46) //sを押す
 						if(y_down <= 0 || x_down <= 0);
 						else {
 							y_down--;
 							x_down--;
 						}
+
+					else if(event.xkey.keycode == 58) {
+						//mを押す
+						if(mol >= MAX_BALL);
+						else mol++;
+					} else if(event.xkey.keycode == 41) {
+						//fを押す
+						if(mol <= 0);
+						else mol--;
+					} else if(event.xkey.keycode == 43) {
+						//hを押す
+						T++;
+						if(y_up - y_down <= large + (MAX_Speed * T / 100) * 2 || x_up - x_down <= large + (MAX_Speed * T
+							/ 100) * 2) {
+							y_down = y_up - large - (MAX_Speed * T / 100) * 2;
+							x_down = x_up - large - (MAX_Speed * T / 100) * 2;
+						}
+					} else if(event.xkey.keycode == 54) {
+						//cを押す
+						if(T <= 1);
+						else T--;
+					}
 
 					for(int i = 9; i > 0; i--) {
 						command[i] = command[i - 1];
@@ -109,60 +128,64 @@ void main(int argc, char** argv) {
 					// 挿入
 					command[0] = event.xkey.keycode;
 
-					if(command[0] == 38 && command[1] == 56 && command[2] == 114 &&
-						command[3] == 113 && command[4] == 114 && command[5] == 113 &&
-						command[6] == 116 && command[7] == 116 && command[8] == 111 &&
+					if(command[0] == 38 && //上上下下左右左右BASを押す
+						command[1] == 56 &&
+						command[2] == 114 &&
+						command[3] == 113 &&
+						command[4] == 114 &&
+						command[5] == 113 &&
+						command[6] == 116 &&
+						command[7] == 116 &&
+						command[8] == 111 &&
 						command[9] == 111) {
 						mode = 2;
-					} else
-						mode = 1;
+					} else mode = 1;
 					break;
 				default:
 					break;
 			}
 		}
 
-		// 背景クリア
+
+		//背景クリア
 		XClearWindow(dpy, w);
 
-		// 枠の描画
+		//枠の描画
 		XSetForeground(dpy, gc, black);
 		XDrawRectangle(dpy, w, gc, x_down, y_down, x_up - x_down, y_up - y_down);
 
-		// それぞれの値の出力
-		sprintf(p_value, "P = %.2f*10**3 [Pa]",
-		        (float)(BALL * R * T / ((x_up - x_down) * (y_up - y_down))));
-		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up - 150,
-		                  (XftChar8*)p_value, strlen(p_value));
+		//それぞれの値の出力
+		sprintf(p_value, "P = %.2f*10**3 [Pa]", (float)(mol * R * T / ((x_up - x_down) * (y_up - y_down))));
+		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up - 200, (XftChar8*)p_value, strlen(p_value));
 
 		sprintf(v_value, "V = %d [m**3]", (int)(x_up - x_down) * (y_up - y_down));
-		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up - 100,
-		                  (XftChar8*)v_value, strlen(v_value));
+		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up - 150, (XftChar8*)v_value, strlen(v_value));
 
-		sprintf(n_value, "n = %d [mol]", (int)(BALL));
-		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up - 50,
-		                  (XftChar8*)n_value, strlen(n_value));
+		sprintf(n_value, "n = %d [mol]", (int)(mol));
+		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up - 100, (XftChar8*)n_value, strlen(n_value));
+
+		sprintf(r_value, "R = %.1f [Pa*m**3/mol*K]", (float)(R));
+		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up - 50, (XftChar8*)r_value, strlen(r_value));
 
 		sprintf(t_value, "T = %d [K]", (int)(T));
-		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up,
-		                  (XftChar8*)t_value, strlen(t_value));
+		XftDrawStringUtf8(xftdraw, &xftcolor, xftfont, x_up + 100, y_up, (XftChar8*)t_value, strlen(t_value));
 
-		// ボールの更新
-		for(int i = 0; i < BALL + 1; i++) {
-			if(x_prot[i] < x_down + MAX_Speed) {
-				x_prot[i] = x_down + MAX_Speed;
+		//ボールの更新
+		for(int i = 0; i < MAX_BALL + 1; i++) {
+			if(x_prot[i] < x_down + MAX_Speed * T / 100) {
+				x_prot[i] = x_down + MAX_Speed * T / 100;
 				x_vector[i] *= -1;
 			}
-			if(x_prot[i] > x_up - large - MAX_Speed) {
-				x_prot[i] = x_up - large - MAX_Speed;
+			if(x_prot[i] > x_up - large - MAX_Speed * T / 100) {
+				x_prot[i] = x_up - large - MAX_Speed * T / 100;
 				x_vector[i] *= -1;
 			}
-			if(y_prot[i] < y_down + MAX_Speed) {
-				y_prot[i] = y_down + MAX_Speed;
+			if(y_prot[i] < y_down + MAX_Speed * T / 100) {
+				y_prot[i] = y_down + MAX_Speed * T / 100;
 				y_vector[i] *= -1;
 			}
-			if(y_prot[i] > y_up - large - MAX_Speed) {
-				y_prot[i] = y_up - large - MAX_Speed;
+			if(y_prot[i] > y_up - large - MAX_Speed * T / 100) {
+				y_prot[i] = y_up - large - MAX_Speed * T / 100;
 				y_vector[i] *= -1;
 			}
 			ball(x_vector[i], y_vector[i], i);
@@ -174,19 +197,15 @@ void main(int argc, char** argv) {
 }
 
 void ball(int x_vector, int y_vector, int number) {
-	x_prot[number] += x_vector * (rand() % MAX_Speed + 1);
-	y_prot[number] += y_vector * (rand() % MAX_Speed + 1);
+	char message[] = "DVD";
+
+	x_prot[number] += x_vector * (rand() % (int)(MAX_Speed * T / 100 + 1));
+	y_prot[number] += y_vector * (rand() % (int)(MAX_Speed * T / 100 + 1));
 
 	if(mode == 1) {
-		if(number < BALL)
-			XDrawArc(dpy, w, gc, x_prot[number], y_prot[number], large, large, 0,
-			         360 * 64);
+		if(number < mol)XDrawArc(dpy, w, gc, x_prot[number], y_prot[number], large, large, 0, 360 * 64);
 	} else {
-		if(number < BALL)
-			XDrawArc(dpy, w, gc, x_prot[number], y_prot[number], large, large, 0,
-			         360 * 64);
-		else
-			XDrawString(dpy, w, gc, x_prot[number], y_prot[number], message,
-			            strlen(message));
+		if(number < mol)XDrawArc(dpy, w, gc, x_prot[number], y_prot[number], large, large, 0, 360 * 64);
+		else if(number == mol) XDrawString(dpy, w, gc, x_prot[number], y_prot[number], message, strlen(message));
 	}
 }
--- test.c
+++ test.c
@@ -1,13 +1,24 @@
-/* xdraw_sin.c */
-#include <stdio.h>
-#include <math.h>    // cc -lm
-#include <X11/Xlib.h> // cc -l X11
-#include <X11/Xutil.h>
-#include <unistd.h>
+// ----- Parameters -----
+#define BORDER 3 // ウィンドウの枠の太さ [px]
+#define WIDTH  700 // ウィンドウの幅 [px]
+#define HEIGHT 700 // ウィンドウの高さ [px]
+#define g 9.80665 // 重力加速度 [px/s^2]
+// #define g 0
+#define e 0.88 // 反発係数
+#define t 0.15 // 1ループでの経過時間 [s]
+#define SLEEP_TIME 8000 // [μs]
+#define R_C 80 // 各速度の割合 (角速度 = v / R_C)
+#define R 30 // ボールの半径 [px]
+#define BALL_MAX 10 // ボールの最大数
+#define WALL_MAX 10 // 壁の最大数
+#define MAX_SPEED 1000 // 最大速度 [px/s]
+// ----------------------
 
-#define BORDER 2
-#define WIDTH  500
-#define HEIGHT 500
+#include <stdio.h>
+#include <math.h>     // cc -lm
+#include <X11/Xlib.h> // cc -l X11
+#include <unistd.h>
+#include <stdbool.h>
 
 #define MOUSE_LEFT 1
 #define MOUSE_MIDDLE 2 // wheel click
@@ -25,122 +36,416 @@
 
 #define PI 3.1415926535
 
-void rotate_point(XPoint *p, XPoint center, double rad) { //chatgpt
-    double dx = p->x - center.x;
-    double dy = p->y - center.y;
+#define red 0xFF0000
+#define gray 0xBEBEBE
+#define white 0xFFFFFF
+#define black 0x000000
 
-    p->x = (short)(dx * cos(-rad) - dy * sin(-rad) + center.x);
-    p->y = (short)(dx * sin(-rad) + dy * cos(-rad) + center.y);
+typedef struct {
+	float x;
+	float y;
+} Vector;
+
+float length(const Vector v) {
+	return sqrtf(powf(v.x, 2) + powf(v.y, 2));
 }
 
-int main(int argc, char **argv) {
+Vector normalize(const Vector v) {
+	const float l = length(v);
+	if(l == 0) return (Vector) {0, 0};
+	return (Vector){v.x / l, v.y / l};
+}
 
-	Display *dpy = XOpenDisplay("");
-	int screen = DefaultScreen(dpy);
-	unsigned long white = WhitePixel(dpy, screen);
-	unsigned long black = BlackPixel(dpy, screen);
-	Window root = DefaultRootWindow(dpy);
-	Window w = XCreateSimpleWindow(dpy, root, 100, 100, WIDTH, HEIGHT, BORDER, black, white);
+// 追加: ベクトル基本
+Vector add(const Vector a, const Vector b){ return (Vector) {a.x+b.x, a.y+b.y}; }
+Vector sub(const Vector a, const Vector b){ return (Vector) {a.x-b.x, a.y-b.y}; }
+Vector mul(const Vector a, const float s) { return (Vector) {a.x*s, a.y*s}; }
+
+float dot(const Vector a, const Vector b) {
+	return a.x * b.x + a.y * b.y;
+}
+// 追加: 線分ABへの最近点
+Vector closest_point(const Vector A, const Vector B, const Vector P) {
+	const Vector AB = sub(B, A);
+	const float ab2 = dot(AB, AB);
+	float t_ = dot(sub(P, A), AB) / ab2;
+	t_ = t_ < 0 ? 0 : (t_ > 1 ? 1 : t_);
+	return add(A, mul(AB, t_));
+}
+
+typedef struct {
+	Vector pos1;
+	Vector pos2;
+	float a;
+	float b;
+	float c; // ax  - y + c = 0
+	Vector n; // 単位法線ベクトル
+} Wall;
+
+typedef struct {
+	Vector center; // 中心座標
+	Vector v; // 速度ベクトル
+	Vector a; // 加速度ベクトル
+	float r_speed; // 回転速度
+	float r; // 半径
+	float rad; // 角度
+	float mass; // 質量
+} Ball;
+
+int ball_last = 0;
+Ball placed_balls[BALL_MAX];
+
+int wall_last = 0;
+Wall placed_walls[WALL_MAX];
+
+Ball* addBall(const Vector center, const Vector v, const Vector a, const float r_speed, const float r, const float rad, const float mass) {
+	const Ball ball = {center, v, a, r_speed, r, rad, mass};
+	if(++ball_last >= BALL_MAX) ball_last = 0;
+	placed_balls[ball_last] = ball;
+	return placed_balls + ball_last;
+}
+
+Wall* addWall(Vector pos1, Vector pos2) {
+	if(pos1.x > pos2.x) {
+		const Vector temp = pos1;
+		pos1 = pos2;
+		pos2 = temp;
+	}
+
+	const float dx = pos2.x - pos1.x,
+				dy = pos2.y - pos1.y;
+	float a, b; // ax - y + c = 0
+	if(dx < 0 || 0 < dx) {
+		// dx != 0
+		a = dy / dx; // y = ax+c
+		b = -1;
+	} else {
+		// x = c
+		a = 1;
+		b = 0;
+	}
+	const float c = pos1.y - a * pos1.x;
+	if(!(dy < 0 || 0 < dy)) {
+		// dy == 0 => y + c = 0
+		a = 0;
+		b = 1;
+	}
+
+	Vector n = (Vector) {dy, -dx};
+	const float l = length(n);
+	n = (Vector){n.x / l, n.y / l}; // 点は考慮しない
+	const Wall w = {pos1, pos2, a, b, c, n};
+	printf("Wall generated:\n\ta = %f, b = %f, c = %f, n = (%f, %f)\n", a, b, c, n.x, n.y);
+	if(++wall_last >= WALL_MAX) wall_last = 0;
+	placed_walls[wall_last] = w;
+	return placed_walls + wall_last;
+}
+
+void rotate_point(Vector* p, const Vector center, const float rad) {
+	const float dx = p->x - center.x,
+				dy = p->y - center.y;
+	p->x = dx * cosf(rad) - dy * -sinf(rad) + center.x;
+	p->y = dx * -sinf(rad) + dy * cosf(rad) + center.y;
+}
+
+void DrawBall(Display* dpy, const Window w, GC gc, Ball* ball) {
+	if(ball->r <= 0) return;
+	const float dr = ball->r * 2; // 直径
+	const Vector pos = {ball->center.x - (ball->r), ball->center.y - (ball->r)};
+	float deg = (float)((180 * ball->rad) / PI);
+	while(deg > 360) deg -= 360; while(deg < 0) deg += 360;
+
+	XSetForeground(dpy, gc, red);
+	XFillArc(dpy, w, gc, (int)pos.x, (int)pos.y, (int)dr, (int)dr, (int)(deg * 64.0), 180 * 64);
+
+	XSetForeground(dpy, gc, gray);
+
+	float round = 180 + deg;
+	while(round > 360) round -= 360; while(round < 0) round += 360;
+	XFillArc(dpy, w, gc, (int)pos.x, (int)pos.y, (int)dr, (int)dr, (int)(round * 64.0), 180 * 64);
+
+	XSetForeground(dpy, gc, black);
+
+	Vector vpoint[4];
+	vpoint[0] = (Vector){pos.x, pos.y + (ball->r) - (ball->r / 15)};
+	vpoint[1] = (Vector){pos.x + dr, vpoint[0].y};
+	vpoint[2] = (Vector){vpoint[1].x, vpoint[0].y + (dr / 15)};
+	vpoint[3] = (Vector){vpoint[0].x, vpoint[2].y};
+
+	for(int i = 0; i < 4; i++) {
+		rotate_point(&vpoint[i], ball->center, ball->rad);
+	}
+
+	XPoint point[4];
+	for(int i = 0; i < 4; i++) {
+		point[i].x = (short)vpoint[i].x;
+		point[i].y = (short)vpoint[i].y;
+	}
+
+	XFillPolygon(dpy, w, gc, point, 4, Convex, CoordModeOrigin);
+
+	XSetForeground(dpy, gc, black); // 中心外側円
+	XFillArc(dpy, w, gc, (int)(pos.x + (ball->r) - (ball->r / 5)), (int)(pos.y + (ball->r) - (ball->r / 5)),
+	         (int)dr / 5, (int)dr / 5, 0, 360 * 64);
+
+	XSetForeground(dpy, gc, white); // 中心内側円
+	XFillArc(dpy, w, gc, (int)(pos.x + (ball->r * 5 / 6)), (int)(pos.y + (ball->r * 5 / 6)),
+	         (int)((dr / 5) - (ball->r / 15)), (int)((dr / 5) - (ball->r / 15)), 0, 360 * 64);
+	// XFillArc(dpy, w, gc, (int) (pos.x+(ball->r)-(ball->r/5)+(ball->r/30)), (int) (pos.y+(ball->r)-(ball->r/5)+(ball->r/30)), (int) ((dr/5)-(ball->r/15)), (int) ((dr/5)-(ball->r/15)), 0, 360*64);
+
+	// XSetForeground(dpy, gc, black); // 外接正角形
+	// XDrawRectangle(dpy, w, gc, (int)pos.x, (int)pos.y, (int)dr, (int)dr);
+
+	while(ball->rad > 2 * PI) ball->rad -= (float)(2 * PI); while(ball->rad < 0) ball->rad += (float)(2 * PI);
+}
+
+void DrawWall(Display* dpy, const Window w, GC gc, const Wall* wall) {
+	if(wall->pos1.x == -1) return;
+	XSetForeground(dpy, gc, black);
+	XDrawLine(dpy, w, gc, (int)wall->pos1.x, (int)wall->pos1.y, (int)wall->pos2.x, (int)wall->pos2.y);
+}
+
+void DrawWalls(Display* dpy, const Window w, GC gc) {
+	for(int i = 0; i < WALL_MAX; ++i)
+		DrawWall(dpy, w, gc, placed_walls + i);
+}
+
+void DrawBalls(Display* dpy, const Window w, GC gc) {
+	for(int i = 0; i < BALL_MAX; ++i)
+		DrawBall(dpy, w, gc, placed_balls + i);
+}
+
+// reflect() を「最近点」方式に差し替え
+void reflect(const Ball before, Ball* after) {
+
+	for(int i = 0; i < WALL_MAX; ++i) {
+		const Wall w = placed_walls[i];
+		if(w.pos1.x == -1) continue;
+
+		// before/after の中心から最近点を取る（貫通対策で両方見る）
+		const Vector P_before = closest_point(w.pos1, w.pos2, before.center);
+		const Vector P_after  = closest_point(w.pos1, w.pos2, after->center);
+
+		const Vector d_after = sub(after->center, P_after);
+		const float dist_after = length(d_after);
+
+		// 非接触ならスキップ
+		if(dist_after > after->r) {
+			// ただし「高速で貫通」して after では離れているケースがあるので
+			// before側が接触していた/跨いだ可能性も軽く見る
+			const Vector d_before = sub(before.center, P_before);
+			const float dist_before = length(d_before);
+			if(dist_before > before.r) continue;
+		}
+
+		// 法線: 最近点→中心（壁から外向き）
+		Vector n = normalize(d_after);
+		if(n.x == 0.0f && n.y == 0.0f) {
+			// 中心がちょうど最近点にある等で法線が作れない場合、壁の法線っぽいものを作る
+			const Vector AB = sub(w.pos2, w.pos1);
+			n = normalize((Vector){-AB.y, AB.x});
+			if(n.x == 0.0f && n.y == 0.0f) continue;
+		}
+
+		// めり込み補正（中心を壁外へ）
+		const float penetration = after->r - dist_after;
+		if(penetration > 0) {
+			after->center = add(after->center, mul(n, penetration));
+		}
+
+		// 反射: 壁へ向かっているときだけ
+		const float vn = dot(after->v, n);
+		if(vn < 0) {
+			after->v.x -= (float)(1.0f + e) * vn * n.x;
+			after->v.y -= (float)(1.0f + e) * vn * n.y;
+		}
+	}
+}
+
+int main(int argc, char** argv) {
+	for(int i = 0; i < BALL_MAX; i++) {
+		placed_balls[i] = (Ball){
+			(Vector){-1, -1},
+			(Vector){-1, 1},
+			(Vector){-1, -1},
+			-1, -1, -1, -1
+		};
+	} // init
+
+	for(int i = 0; i < WALL_MAX; i++) {
+		placed_walls[i] = (Wall){
+			(Vector){-1, -1},
+			(Vector){-1, -1}
+		};
+	} // init
+
+	Display* dpy = XOpenDisplay("");
+	const Window root = DefaultRootWindow(dpy),
+	             w = XCreateSimpleWindow(dpy, root, 100, 100, WIDTH, HEIGHT, BORDER, black, white);
 	XMapWindow(dpy, w);
 	GC gc = XCreateGC(dpy, w, 0, NULL);
 
-	Colormap cmap = DefaultColormap(dpy, 0);       /* カラーマップ構造体 */
-	XColor c0, red, gray, green, blue;             /* カラー構造体 */
-	XAllocNamedColor(dpy, cmap, "red", &red, &c0); /* COLOUR.pixel */
-	XAllocNamedColor(dpy, cmap, "gray", &gray, &c0);
-	XAllocNamedColor(dpy, cmap, "Light green", &green, &c0);
-	XAllocNamedColor(dpy, cmap, "blue", &blue, &c0);
-
-	unsigned long colors[5] = {black, red.pixel, gray.pixel, green.pixel, blue.pixel};
-
-	Window exit = XCreateSimpleWindow(dpy, w, 10, 10, 50, 30, BORDER, black, white); // exit window
+	const Window exit = XCreateSimpleWindow(dpy, w, 10, 10, 50, 30, BORDER, black, gray); // exit window
 	XMapSubwindows(dpy, w);
 
-	XSelectInput(dpy, w, PointerMotionMask);
-	XSelectInput(dpy, exit, ButtonPressMask | ExposureMask);
+	XSelectInput(dpy, w, PointerMotionMask | ButtonPressMask | ButtonReleaseMask);
+	XSelectInput(dpy, exit, ButtonPressMask);
 
 	XEvent event;
 
-	int pos_x = -1, pos_y = -1; //init
-	int s_pos_x = -1, s_pos_y = -1; // init
+	Vector start = {-1, -1},
+	       mouse = {-1, -1};
 
-	unsigned long current = black;
+	bool isWritingArrow = false,
+	     isWritingWall = false;
 
-	char showing_letter[9];
-	KeySym key;
-	char string[9];
+	addWall((Vector){0, 0}, (Vector){WIDTH, 0}); // 上
+	addWall((Vector){0, 0}, (Vector){0, HEIGHT}); // 左
+	addWall((Vector){WIDTH, 0}, (Vector){WIDTH, HEIGHT}); // 右
+	addWall((Vector){0, HEIGHT}, (Vector){WIDTH, HEIGHT}); // 下
 
-	int x = 100;
-	int y = 100;
-	int r = 50;
-	XPoint center = {x+(r/2), y+(r/2)};
-
-	float deg = 0;
-	float rad = 0;
-
-	while(1){
-		if(XEventsQueued(dpy, QueuedAfterReading)){
+	while(1) {
+		if(XEventsQueued(dpy, QueuedAfterReading)) {
 			XNextEvent(dpy, &event);
-			switch(event.type){
+			switch(event.type) {
 				case MotionNotify:
-					x = event.xmotion.x;
-					y = event.xmotion.y;
-					center.x = x+(r/2);
-					center.y = y+(r/2);
-				break;
+					if(isWritingArrow) {
+						mouse.x = (float)event.xmotion.x;
+						mouse.y = (float)event.xmotion.y;
+						rotate_point(&mouse, start, PI);
+					}
+					if(isWritingWall) {
+						mouse.x = (float)event.xmotion.x;
+						mouse.y = (float)event.xmotion.y;
+					}
+					break;
 				case ButtonPress:
 					if(event.xany.window == exit) return 0;
-					//
-				break;
-				case Expose:
-					XDrawString(dpy, exit, gc, 10, 18, "Exit", 4);
-				break;
+					if(event.xany.window == w) {
+						switch(event.xbutton.button) {
+							case MOUSE_LEFT:
+								if(isWritingWall) continue;
+								start.x = (float)event.xbutton.x;
+								start.y = (float)event.xbutton.y;
+								mouse.x = start.x;
+								mouse.y = start.y;
+								const float mass = 5;
+								addBall(start, (Vector){0, 0}, (Vector){0, 0}, 0, R, 0, mass);
+								isWritingArrow = true;
+								break;
+							case MOUSE_RIGHT:
+								if(isWritingArrow) continue;
+								start.x = (float)event.xbutton.x;
+								start.y = (float)event.xbutton.y;
+								isWritingWall = true;
+								break;
+							default:
+								continue;
+						}
+					}
+					break;
+				case ButtonRelease:
+					if(event.xany.window == w) {
+						switch(event.xbutton.button) {
+							case MOUSE_LEFT:
+								if(isWritingArrow) {
+									isWritingArrow = false;
+									Vector end = {(float)event.xbutton.x, (float)event.xbutton.y};
+									rotate_point(&end, start, PI);
+									placed_balls[ball_last].v.x = (end.x - start.x) / placed_balls[ball_last].mass;
+									placed_balls[ball_last].v.y = (end.y - start.y) / placed_balls[ball_last].mass;
+									placed_balls[ball_last].r_speed = placed_balls[ball_last].v.x / R_C;
+									placed_balls[ball_last].a.y = g;
+									mouse.x = -1;
+								}
+								break;
+							case MOUSE_RIGHT:
+								if(isWritingWall) {
+									isWritingWall = false;
+									addWall(start, (Vector){(float)event.xbutton.x, (float)event.xbutton.y});
+								}
+								break;
+							case MOUSE_MIDDLE:
+								for(int i = 0; i < BALL_MAX; i++) {
+									placed_balls[i] = (Ball){
+										(Vector){-1, -1},
+										(Vector){-1, 1},
+										(Vector){-1, -1},
+										-1, -1, -1, -1
+									};
+								} // init
+								for(int i = 0; i < WALL_MAX; i++) {
+									placed_walls[i] = (Wall){
+										(Vector){-1, -1},
+										(Vector){-1, -1}
+									};
+								} // init
+								break;
+							default:
+								continue;
+						}
+					}
+					break;
+				default:
+					continue;
 			}
 		} else {
-
-			float pos = 180.0+deg;
-			if(pos > 360) pos -= 360;
-
+			usleep(SLEEP_TIME);
 			XClearWindow(dpy, w);
 
-			XSetForeground(dpy, gc, red.pixel);
-			XFillArc(dpy, w, gc, x, y, r, r, deg*64.0, 180*64);
+			for(int i = 0; i < BALL_MAX; ++i) {
+				Ball* ball = placed_balls + i;
+				if(ball->r <= 0) continue;
+				const Ball before = *ball;
 
-			XSetForeground(dpy, gc, gray.pixel);
-			XFillArc(dpy, w, gc, x, y, r, r, pos*64, 180*64);
+				ball->v.x += ball->a.x * (float)t;
+				ball->v.y += ball->a.y * (float)t;
+				ball->center.x += ball->v.x * (float)t;
+				ball->center.y += ball->v.y * (float)t;
+				ball->rad -= ball->r_speed * (float)t;
+				while(ball->rad > 2 * PI) ball->rad -= (float)(2 * PI);
+				while(ball->rad < 0) ball->rad += (float)(2 * PI);
+				reflect(before, ball);
+				if(isfinite(ball->center.x) == false || isfinite(ball->center.y) == false) {
+					placed_balls[i] = (Ball){
+						// ボールを削除
+						(Vector){-1, -1},
+						(Vector){-1, 1},
+						(Vector){-1, -1},
+						-1, -1, -1, -1
+					};
+					continue;
+				}
 
-			XSetForeground(dpy, gc, black);
-			XPoint point[4];
-			point[0].x = x;
-			point[0].y = y+(r/2)-(r/30);
-			point[1].x = x+r;
-			point[1].y = y+(r/2)-(r/30);
-			point[2].x = x+r;
-			point[2].y = y+(r/2)-(r/30)+(r/15);
-			point[3].x = x;
-			point[3].y = y+(r/2)-(r/30)+(r/15);
-			float rad = PI*deg / 180.0; 
-			rotate_point(point, center, rad);
-			rotate_point(point+1, center, rad);
-			rotate_point(point+2, center, rad);
-			rotate_point(point+3, center, rad);
-			XFillPolygon(dpy, w, gc, point, 4, Convex, CoordModeOrigin);
+				if(ball->center.x < 0 || ball->center.y < 0) {
+					placed_balls[i] = (Ball){
+						(Vector){-1, -1},
+						(Vector){-1, 1},
+						(Vector){-1, -1},
+						-1, -1, -1, -1
+					}; // ボールを削除
+					continue;
+				}
 
-			XSetForeground(dpy, gc, black);// 中心外側円
-			XFillArc(dpy, w, gc, x+(r/2)-(r/10), y+(r/2)-(r/10), r/5, r/5, 0, 360*64);
+				if(ball->v.x > MAX_SPEED) {
+					ball->v.x = MAX_SPEED;
+				} else if(ball->v.y > MAX_SPEED) {
+					ball->v.y = MAX_SPEED;
+				}
 
-			XSetForeground(dpy, gc, white);// 中心内側円
-			XFillArc(dpy, w, gc, x+(r/2)-(r/10)+(r/60), y+(r/2)-(r/10)+(r/60), (r/5)-(r/30), (r/5)-(r/30), 0, 360*64);
+				ball->r_speed = ball->v.x / R_C;
 
-			// XSetForeground(dpy, gc, black);// 外接正角形
-			// XDrawRectangle(dpy, w, gc, x, y, r, r); 
-			// XDrawLine(dpy, w, gc, x+(r/2), y, x+(r/2), y+r);
+			}
 
-			deg += 0.03;
-			if(deg > 360) deg = 0.0;
-			usleep(500);
+			DrawBalls(dpy, w, gc);
+			DrawWalls(dpy, w, gc);
+
+			if((isWritingArrow || isWritingWall) && mouse.x != -1) {
+				XSetForeground(dpy, gc, black);
+				XDrawLine(dpy, w, gc, (int)start.x, (int)start.y, (int)mouse.x, (int)mouse.y);
+			}
+
 			XFlush(dpy);
-
 		}
 	}
 }
